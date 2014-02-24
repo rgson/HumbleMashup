67459b8202c7c5e7ee7b742175ed6e41f14f54d7
syvjohan <johanfredriksson1987@gmail.com>
2014-02-24 13:51:32 +0100

    Implemented constructor and get set attributes

--- api/includes/classes/Game.php
+++ api/includes/classes/Game.php
@@ -2,6 +2,61 @@
 
 class Game {
 
+	private $title;
+	private $price;
+	private $score;
+	private $appid;
+	private $owned;
+
+	public function __construct($title, $picture, $score, $appid, $owned) {
+		$this->title = (string) $title;
+		$this->picture = (string) $picture;
+		$this->score = (float) $score;
+		$this->appid = (string) $appid;
+		$this->owned = (bool) $owned;
+	}
+
+	// Title get, set attribut
+	function setTitle($newTitle) {
+		$this->title = $newTitle;
+	}
+
+	//Picture get, set attribut
+	function setPicture($newPicture) {
+		$this->picture = $newPicture;
+	}
+
+	function getPicture() {
+		return $this->picture;
+	}
+
+	//Score get, set attribut
+	function setScore($newScore) {
+		$this->score = $newScore;
+	}
+
+	function getScore() {
+		return $this->Score;
+	}
+
+	//Appid get, set attribut
+	function setAppid($newAppid) {
+		$this->appid = $newAppid;
+	}
+
+	function getAppid() {
+		return $this->appid;
+	}
+
+	//Owned get, set attribut
+	function setOwned($newOwned) {
+		$this->owned = $newOwned;
+	}
+
+	function getOwned() {
+		return $this->owned;
+	}
+
 	//TODO Member variables: title, price, score, appid, owned
 	
 	//TODO Magic method __get for url and picture
