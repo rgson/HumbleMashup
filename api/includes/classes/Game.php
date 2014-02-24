6db6ebfc9aa6cb28107ca10777afc52ec2c43916
syvjohan <johanfredriksson1987@gmail.com>
2014-02-24 14:09:38 +0100

    jsonSerialize

--- api/includes/classes/Game.php
+++ api/includes/classes/Game.php
@@ -56,13 +56,8 @@ class Game {
 	function getOwned() {
 		return $this->owned;
 	}
-
-	//TODO Member variables: title, price, score, appid, owned
 	
 	//TODO Magic method __get for url and picture
-	
-	//TODO Constructor?
-
 }
 
 ?>
