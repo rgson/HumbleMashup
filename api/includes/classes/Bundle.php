0411b6ab714f8436df8c7fe3fc3a1b1398fb58b9
syvjohan <johanfredriksson1987@gmail.com>
2014-02-24 13:02:51 +0100

    added "propertys" and constructor

--- api/includes/classes/Bundle.php
+++ api/includes/classes/Bundle.php
@@ -1,19 +1,56 @@
 <?php
 
-class Bundle extends HumbleBundle {
+class Bundle {
 
-	public function GetBundleInfo {
+	private $title:
+	private $picture;
+	private $url;
+	private $games;
 
-			/*$humbleBundle = new HumbleBundle();
-
-			try {
-				humbleBundle->getHumbleBundle();
-			} catch (exception $e) {
-				APIOutput::http_response("No response from HumbleBundle.php'.");
-			}
-
-			return humbleBundle;*/
+	public function Construct($title, $picture, $url, $games)
+	{
+		$this->title = (string) $title;
+		$this->picture = (string) $picture;
+		$this->url = (string) $url;
+		$this->games = array();
 	}
+
+	// Title get, set attribut
+	function SetTitle($newTitle) {
+		this->title = $newTitle;
+	}
+
+	function GetTitle() {
+		return $this->title;
+	}
+
+	//Picture get, set attribut
+	function SetPicture($newPicture) {
+		this->picture = $newPicture;
+	}
+
+	function GetPicture() {
+		return $this->picture;
+	}
+
+	// Url get, set attribut
+	function SetUrl($newUrl) {
+		this->url = $newUrl;
+	}
+
+	function GetUrl() {
+		return $this->url;
+	}
+
+	// Games get, set attribut
+	function SetGames($newGames) {
+		this->games[] = $newGames;
+	}
+
+	function GetGames() {
+		return $this->games[];
+	}
+
 	//Stub.
 	//TODO Member variables for title, picture, url and games.
 }
