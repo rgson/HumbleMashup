3d68217d83be838874105abaf74cde8602bf99d6
syvjohan <johanfredriksson1987@gmail.com>
2014-02-24 11:50:02 +0100

    start implement class

--- api/includes/classes/Bundle.php
+++ api/includes/classes/Bundle.php
@@ -1,10 +1,21 @@
 <?php
 
-class Bundle {
+class Bundle extends HumbleBundle {
 
+	public function GetBundleInfo {
+
+			/*$humbleBundle = new HumbleBundle();
+
+			try {
+				humbleBundle->getHumbleBundle();
+			} catch (exception $e) {
+				APIOutput::http_response("No response from HumbleBundle.php'.");
+			}
+
+			return humbleBundle;*/
+	}
 	//Stub.
 	//TODO Member variables for title, picture, url and games.
-
 }
 
 ?>
