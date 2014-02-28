6151814f89a650a7ceb4de0601184281dc491d4e
syvjohan <johanfredriksson1987@gmail.com>
2014-02-28 17:01:57 +0100

    added link open in new tab

--- includes/pages/part/footer.php
+++ includes/pages/part/footer.php
@@ -5,8 +5,8 @@
 		<a href="contact" class="icon contact">Contact</a>
 	</div>
 	<div class="column">
-		<a href="http://www.humblebundle.com" class="icon humblebundle">Humble Bundle</a>
-		<a href="http://www.metacritic.com" class="icon metacritic">Metacritic</a>
-		<a href="http://www.steampowered.com" class="icon steam">Steam</a>
+		<a href="http://www.humblebundle.com" target="_blank" class="icon humblebundle">Humble Bundle</a>
+		<a href="http://www.metacritic.com" target="_blank" class="icon metacritic">Metacritic</a>
+		<a href="http://www.steampowered.com" target="_blank" class="icon steam">Steam</a>
 	</div>
 </footer>
